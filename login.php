26d12a0a19a06bc35e7101054e71e4aafe16c7a7
Gautam Kothari <39801903+Gautam5328@users.noreply.github.com>
2020-06-09 22:10:53 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,173 @@
+<?php  
+session_start(); 
+if(isset($_SESSION['admin_sid']) || isset($_SESSION['customer_sid']))
+{
+	header("location:index.php");
+}
+else{
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-tap-highlight" content="no">
+  <title>Login</title>
+
+  <!-- Favicons-->
+  <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
+  <!-- Favicons-->
+  <link rel="apple-touch-icon-precomposed" href="images/favicon/apple-touch-icon-152x152.png">
+  <!-- For iPhone -->
+  <meta name="msapplication-TileColor" content="#00bcd4">
+  <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
+  <!-- For Windows Phone -->
+
+
+  <!-- CORE CSS-->
+  
+  <link href="css/om/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="css/style.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+    <!-- Custome CSS-->    
+    <link href="css/custom/custom.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="css/layouts/page-center.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+  <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+  <link href="js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
+ <style type="text/css"> .no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.se-pre-con {
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+  background: url(pizz.gif) center no-repeat #fff;
+}
+
+nav{
+  float: right;
+}
+
+li{
+  display: inline-block;
+  padding: 1rem;
+}
+
+li a{
+  color: #fff;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.active{
+  color: gold;
+}
+
+</style>
+<script>
+  //paste this code under head tag or in a seperate js file.
+  // Wait for window load
+  $(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+  });
+</script>
+</head>
+
+<body class="red">
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+  
+    <div class="se-pre-con"></div>
+  
+  </div>
+  <!-- End Page Loading -->
+
+    
+    
+
+    <!-- NAVIGATION -->
+    <nav>
+      <ul>
+      <li><a href="homepage.html" class="active">Home</a></li>
+      <li><a href="About.html" class="active"> About Us </a></li>
+      <li>
+        <a href="javascript:history.back()" class="active">Go Back</a>
+      </li>
+
+      <li>
+        <a href="register.php" class="active">SignUp</a>
+      </li>
+      <li>
+        <a href="login.php" class="active">Login</a>
+      </li>
+
+      </ul>
+    </nav>
+    <!-- END OF NAVIGN -->
+  
+
+  <div id="login-page" class="row">
+    <div class="col s12 z-depth-3 card-panel">
+      <form method="post" action="routers/router.php" class="login-form" id="form">
+        <div class="row">
+          <div class="input-field col s12 center">
+            <p class="center login-form-text">Login FOR Pizza Order</p>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-social-person-outline prefix"></i>
+            <input name="username" id="username" type="text">
+            <label for="username" class="center-align">Username</label>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+            <input name="password" id="password" type="password">
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div class="row">
+			<a href="javascript:void(0);" onclick="document.getElementById('form').submit();" class="btn waves-effect waves-light col s12 center">Login</a>
+          </div>
+		  		<div class="row">
+          <div class="input-field col s6 m6 l6">
+            <p class="margin medium-small"><a href="register.php">Register Now!</a></p>
+          </div>         
+        </div>
+        </div>
+
+
+      </form>
+    </div>
+  </div>
+
+
+
+  <!-- ================================================
+    Scripts
+    ================================================ -->
+
+  <!-- jQuery Library -->
+  <script type="text/javascript" src="js/plugins/jquery-1.11.2.min.js"></script>
+  <!--materialize js-->
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <!--scrollbar-->
+  <script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+      <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+    <script type="text/javascript" src="js/plugins.min.js"></script>
+    <!--custom-script.js - Add your own theme custom JS-->
+    <script type="text/javascript" src="js/custom-script.js"></script>
+
+</body>
+</html>
+<?php
+}
+?>
